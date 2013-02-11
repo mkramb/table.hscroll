74d460d6008326b921e6dc977ca7dfcc22ffdef6
Mitja Kramberger <mitja@vizius.si>
2013-02-11 14:23:12 +0100

    change indentation (tabs > spaces)

--- dist/table.hscroll.js
+++ dist/table.hscroll.js
@@ -3,101 +3,101 @@
 * Copyright (c) 2013 Mitja Kramberger; Licensed MIT */
 
 (function($) {
-	var rows = [];
-	var stack = [];
+  var rows = [];
+  var stack = [];
 
-    var settings = {
-        'ignore' : '.ignore',
-        'max' : 4
-    };
+  var settings = {
+    'ignore' : '.ignore',
+    'max' : 4
+  };
 
-    var methods = {
-		initialize: function(options) {
-			settings = $.extend(settings, options);
+  var methods = {
+    initialize: function(options) {
+      settings = $.extend(settings, options);
 
-			$('tr', $(this)).each(function() {
-				rows.push($([
-					'td:not(', settings.ignore, '),',
-					'th:not(', settings.ignore, ')'
-				].join(''), $(this)));
-			});
+      $('tr', $(this)).each(function() {
+        rows.push($([
+          'td:not(', settings.ignore, '),',
+          'th:not(', settings.ignore, ')'
+        ].join(''), $(this)));
+      });
 
-			if (!rows.length) {
-				stack = null;
-				return;
-			}
-			else if (rows[0].length <= settings.max) {
-				stack = null;
-				return;
-			}
+      if (!rows.length) {
+        stack = null;
+        return;
+      }
+      else if (rows[0].length <= settings.max) {
+        stack = null;
+        return;
+      }
 
-			for (var i=0; i<rows.length; i++) {
-				stack.push(rows[i].splice(settings.max, rows[i].length));
-				rows[i] = rows[i].splice(0, settings.max);
+      for (var i=0; i<rows.length; i++) {
+        stack.push(rows[i].splice(settings.max, rows[i].length));
+        rows[i] = rows[i].splice(0, settings.max);
 
-				for (var j = 0; j < stack[i].length; j++) {
-					$(stack[i][j]).remove();
-				}
-			}
-		},
-		moveLeft: function() {
-			$('tr', $(this)).each(function(i) {
-				var element = stack[i].shift(),
-                    first = $([
-						'td:not(', settings.ignore, '):first,',
-                        'th:not(', settings.ignore, '):first'
-                    ].join(''), $(this));
+        for (var j = 0; j < stack[i].length; j++) {
+          $(stack[i][j]).remove();
+        }
+      }
+    },
+    moveLeft: function() {
+      $('tr', $(this)).each(function(i) {
+        var element = stack[i].shift(),
+          first = $([
+            'td:not(', settings.ignore, '):first,',
+            'th:not(', settings.ignore, '):first'
+          ].join(''), $(this));
 
-				stack[i].push(first);
+        stack[i].push(first);
 
-				$([
-					'td:not(', settings.ignore, '):last,',
-					'th:not(', settings.ignore, '):last'
-				].join(''), $(this)).after(element);
+        $([
+          'td:not(', settings.ignore, '):last,',
+          'th:not(', settings.ignore, '):last'
+        ].join(''), $(this)).after(element);
         
-				first.remove();
-			});
-		},
-		moveRight: function() {
-			$('tr', $(this)).each(function(i) {
-				var element = stack[i].shift(),
-                    last = $([
-						'td:not(', settings.ignore, '):last,',
-						'th:not(', settings.ignore, '):last'
-                    ].join(''), $(this));
+        first.remove();
+      });
+    },
+    moveRight: function() {
+      $('tr', $(this)).each(function(i) {
+        var element = stack[i].shift(),
+          last = $([
+			'td:not(', settings.ignore, '):last,',
+            'th:not(', settings.ignore, '):last'
+          ].join(''), $(this));
 
-				stack[i].unshift(last);
+        stack[i].unshift(last);
 
-				$([
-					'td:not(', settings.ignore, '):first,',
-					'th:not(', settings.ignore, '):first'
-				].join(''), $(this)).before(element);
+        $([
+          'td:not(', settings.ignore, '):first,',
+          'th:not(', settings.ignore, '):first'
+        ].join(''), $(this)).before(element);
         
-				last.remove();
-			});
-		},
-		canMove: function() {
-			if (stack && stack.length && stack[0].length) {
-				return true;
-			}
+        last.remove();
+      });
+    },
+    canMove: function() {
+      if (stack && stack.length && stack[0].length) {
+        return true;
+      }
 
-			return false;
-		}
-    };
-	
-    $.fn.hscroll = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
-		}
-		else if (typeof method === 'object' || !method) {
-			return methods.initialize.apply(this, arguments);
-		}
-		else {
-			$.error([
-				'Method ', method,
-				' does not exist on jQuery.tooltip'
-			].join(''));
-		}
-	};
+      return false;
+    }
+  };
+  
+  $.fn.hscroll = function(method) {
+    if (methods[method]) {
+      return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+    }
+    else if (typeof method === 'object' || !method) {
+      return methods.initialize.apply(this, arguments);
+    }
+    else {
+      $.error([
+        'Method ', method,
+        ' does not exist on jQuery.tooltip'
+      ].join(''));
+    }
+  };
 
 }(jQuery));
